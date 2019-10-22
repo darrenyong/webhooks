d56bba78aebb44ff838301863218eff22ceec25b
darrenyong <contact.darrenyong@gmail.com>
2019-10-22 16:39:25 -0700

    Add test route for users

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
 
+const users = require("./routes/api/users")
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
@@ -17,3 +19,5 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
+
+app.use("/api/users", users);
