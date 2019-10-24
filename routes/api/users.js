eba8be3d07fe3303f57a980b73e2c6ba82133efb
darrenyong <contact.darrenyong@gmail.com>
2019-10-23 21:18:54 -0700

    Add salt and hash to user registration password

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,6 +19,16 @@ router.post("/register", (req, res) => {
             email: req.body.email,
             password: req.body.password
           })
+
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                     .then(user => res.json(user))
+                     .catch(err => console.log(err))
+            })
+          })
         }
       })
 })
