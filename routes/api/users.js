9bc1921ef073acc01079dfb654556117aded8a63
darrenyong <contact.darrenyong@gmail.com>
2019-10-24 17:03:23 -0700

    Import keys.js to users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
+const keys = require("../../config/keys");
+
+const router = express.Router();
 
 const User = require("../../models/User")
 
