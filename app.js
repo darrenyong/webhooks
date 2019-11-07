6fe63da2f1a108f5d07611b10591f1ec9b9c59de
darrenyong <contact.darrenyong@gmail.com>
2019-11-06 20:10:26 -0800

    Move calling of Intercom API to own route

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
 const keys = require("./config/keys")
 
+const intercomKey = keys.intercomKey;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-
-const intercomKey = keys.intercomKey;
+const tests = require("./routes/api/tests")
 
 // Start up Server
 app.listen(port, () => {
@@ -31,34 +31,18 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
+// Routes
 app.get("/", (req, res) => {
-  console.log("This is the request");
-  console.log(req.body);
+  // console.log("This is the request");
+  // console.log(req.body);
   // console.log("This is the response");
   // console.log(res);
   res.json({msg: "Hello World"});
 })
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+
+// app.use("/api/users", users);
+// app.use("/api/tweets", tweets);
+app.use("/api/tests", tests);
 
 app.get("/test", (req, res) => {
-  const options = {
-    headers: {
-      Authorization: `Bearer ${intercomKey}`,
-      Accept: "application/json"
-    }
-  }
-  
-  https.get("https://api.intercom.io/admins/", options, (response) => {
-    var raw = "";
-
-    response.on("data", (chunk) => {
-      raw += chunk;
-    });
-
-    response.on("end", () => {
-      let result = JSON.parse(raw);
-      res.json({admins: result.admins});
-    })
-  });
 })
---
+++ routes/api/tests.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const keys = require("../../config/keys");
+const https = require("https");
+
+const intercomKey = keys.intercomKey
+
+router.get("/test", (_, res) => {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${intercomKey}`,
+      Accept: "application/json"
+    }
+  };
+
+  https.get("https://api.intercom.io/admins/", options, response => {
+    var raw = "";
+
+    response.on("data", chunk => {
+      raw += chunk;
+    });
+
+    response
+      .on("end", () => {
+        let result = JSON.parse(raw);
+        res.json({ admins: result.admins });
+      })
+      .on("error", error => {
+        res.json({ message: "Error " + error });
+      });
+  });
+});
+
+module.exports = router;
