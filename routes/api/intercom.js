e3fa0d0ca3d083add6a96a3012fd1365ff5761f0
darrenyong <contact.darrenyong@gmail.com>
2019-11-08 14:04:33 -0800

    Add webhook

--- routes/api/intercom.js
+++ routes/api/intercom.js
@@ -73,28 +73,26 @@ router.get("/test", (_, res) => {
 
 router.post("/webhook-test", (req, res) => {
   let conversationId = req.body.data.item.id;
-  console.log(`This is the conversation Id: ${conversationId}`);
 
   intercomClient.conversations.find({ id: `${conversationId}` }, (result) => {
-    console.log(result.body);
-    // let tag = result.body.tags.tags
+    let tag = result.body.tags.tags
 
-    // if (!tag.length) {
-    //   let note_data = {
-    //     id: `${conversationId}`,
-    //     type: "admin",
-    //     message_type: "note",
-    //     admin_id: intercomAdminId,
-    //     body: "Please tag the conversation! :)",
-    //     assignee_id: 0
-    //   }
+    if (!tag.length) {
+      let note_data = {
+        id: `${conversationId}`,
+        type: "admin",
+        message_type: "note",
+        admin_id: intercomAdminId,
+        body: "Please tag the conversation! :)",
+        assignee_id: 0
+      }
 
-    //   intercomClient.conversations.reply(note_data, (result) => {
-    //     console.log("Note added!");
-    //   })      
-    // } else {
-    //   console.log("This convo has a tag! Congrats!")
-    // }
+      intercomClient.conversations.reply(note_data, (result) => {
+        console.log("Note added!");
+      })      
+    } else {
+      console.log("This convo has a tag! Congrats!")
+    }
   })
 })
 
