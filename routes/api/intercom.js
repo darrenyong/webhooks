c8495448de4aacbe8142b181f5d77cc6bb12055e
darrenyong <contact.darrenyong@gmail.com>
2019-11-08 11:11:28 -0800

    Add Intercom Client npm

--- routes/api/intercom.js
+++ routes/api/intercom.js
@@ -2,45 +2,61 @@ const express = require("express");
 const router = express.Router();
 const keys = require("../../config/keys");
 const https = require("https");
+const Intercom = require("intercom-client");
 
 const intercomKey = process.env.intercomKey || keys.intercomKey
+const intercomClient = new Intercom.Client({ token: `${intercomKey}` });
 
 router.get("/test", (_, res) => {
-  // const options = {
-  //   headers: {
-  //     Authorization: `Bearer ${intercomKey}`,
-  //     Accept: "application/json"
-  //   }
-  // };
+  const test_conversationId = 24434924512;
 
-  // https.get("https://api.intercom.io/admins/", options, response => {
-  //   var raw = "";
+  https.get(`https://api.intercom.io/conversations/${test_conversationId}`, convo_options, response => {
+      var raw = "";
 
-  //   response.on("data", chunk => {
-  //     raw += chunk;
-  //   });
+      response.on("data", chunk => {
+        raw += chunk;
+      });
 
-  //   response
-  //     .on("end", () => {
-  //       let result = JSON.parse(raw);
-  //       res.json({ admins: result.admins });
-  //     })
-  //     .on("error", error => {
-  //       res.json({ message: "Error " + error });
-  //     });
-  // });
+      response.on("end", () => {
+          let result = JSON.parse(raw);
+          let tag = result.tags.tags
+          
+          res.json({ result: result });
+          // If a conversation does not have a tag, open it and add a note telling the user to tag it
+          if (!tag.length) {
+            let note_data = JSON.stringify({
+              "admin_id": 3293893,
+              "body": "Please tag the conversation! :)",
+              "user": {
+                "id": "5d113de67801d7b2d885e59f"
+              }
+            });
+            res.json({ msg: "This convo does not have a tag" });
+
+          } else if (tag.length) {
+            res.json({ msg: "This convo does have a tag! Congrats" });
+          }
+          
+        })
+        .on("error", error => {
+          res.json({ message: "Error " + error });
+        });
+    }
+  );
 });
 
 router.post("/webhook-test", (req, res) => {
   let conversationId = req.body.data.item.id;
 
+  // Options for the Conversation API Call
   const options = {
     headers: {
-      Authorization: `Bearer ${intercomKey} `,
-      Accept: "application/json"
+      "Authorization": `Bearer ${intercomKey} `,
+      "Accept": "application/json"
     }
   };
 
+  // Actual Webhook API
   https.get(`https://api.intercom.io/conversations/${conversationId}`, options, (response) => {
     let raw = ""
 
