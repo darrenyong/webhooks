477ba59f585228f8a837935b42d23a1cf2517c73
darrenyong <contact.darrenyong@gmail.com>
2019-11-12 15:22:26 -0800

    Clean up unused files

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
-// const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// const passport = require("passport");
 const https = require("https");
 
 const app = express();
