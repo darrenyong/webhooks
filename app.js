9f38b78039b38de493a949255b75f9623d897d74
darrenyong <contact.darrenyong@gmail.com>
2019-11-12 16:55:24 -0800

    Cleanup unused dependencies

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
 const keys = require("./config/keys")
 
-// const users = require("./routes/api/users");
 const intercom = require("./routes/api/intercom")
 
 // Start up Server
