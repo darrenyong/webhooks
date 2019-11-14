c9169024fbd3eb4863c99be0e9b1381efb7871e1
darrenyong <contact.darrenyong@gmail.com>
2019-11-14 14:19:14 -0800

    Add CRC check

--- routes/api/twitter.js
+++ routes/api/twitter.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const keys = require("../../config/keys");
+const security = require("../../helpers/security")
+
+const twitterConsumerKey = process.env.TWITTER_CONSUMER_KEY || keys.twitterConsumerKey;
+const twitterConsumerSecret = process.env.TWITTER_CONSUMER_SECRET || keys.twitterConsumerSecret;
+const twitterAccessKey = process.env.TWITTER_ACCESS_KEY || keys.twitterAccessKey;
+const twitterAccessSecret = process.env.TWITTER_ACCESS_SECRET || keys.twitterAccessSecret;
 
 router.get("/test", (_, res) => {
   res.json({ msg: "This is the twitter test" });
+})
+
+// Set up Challenge-Response Checks - https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/securing-webhooks
+router.get("/webhook", (req, res) => {
+  var crc_token = req.query.crc_token;
+
+  if (crc_token) {
+    var hash = security.get_challenge_response(crc_token, twitterConsumerSecret)
+
+    res.status(200);
+    res.send({
+      response_token: "sha256=" + hash
+    }) 
+  } else {
+    res.status(400);
+    res.send( "Error - CRC Token missing" )
+  }
+})
+
+router.post("/webhook", (req, res) => {
+  return res.json({ });
 })
 
 module.exports = router;
